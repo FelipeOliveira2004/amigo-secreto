c081ab0e43943ca41ae482ec1d8e09662f7f9bf2
Felipe Oliveira <felipeo2015123@gmail.com>
2025-02-05 20:57:16 -0300

    Projeto parcialmente pronto

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let listaAmigos = [];
+
+function adicionarAmigo() {
+    let amigoSecreto = document.querySelector('input').value;
+    if (amigoSecreto == ''){
+        alert('Por favor, insira um nome.');
+    } else if (verificaLista(amigoSecreto) == true) {
+        return;
+    } else {
+        atualizaLista();
+    }
+    limparCampo();
+}
+
+function atualizaLista() {
+    let lista = document.querySelector('.name-list');
+    lista.innerHTML = '';
+    for (let i = 0; i < listaAmigos.length; i++) {
+        lista.innerHTML += `<li>${listaAmigos[i]}</li>`;
+    }
+}
+
+function limparCampo() {
+    amigoSecreto = document.querySelector('input').value = '';
+}
+
+function verificaLista(amigoSecreto) {
+    let lista = document.querySelector('.name-list');
+    if (lista.innerHTML.includes(amigoSecreto)){
+        alert('O nome já está na lista!');
+        return;
+    } else {
+        listaAmigos.push(amigoSecreto);
+    }
+}
+
+function sortearAmigo(listaAmigos, lista) {
+    let resultadoSorteio = document.querySelector('.result-list');
+    let amigoSorteado = Math.floor(Math.random() * listaAmigos.length);
+    lista.innerHTML = '';
+    resultadoSorteio.innerHTML = `${amigoSorteado}`;
+}
